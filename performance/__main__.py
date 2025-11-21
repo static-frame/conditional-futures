7853ad6536cdd18d23b4225deb2c4a2f5188561f
Christopher Ariza <ariza@flexatone.com>
2025-11-20 18:25:30 -0800

    upated tests

--- performance/__main__.py
+++ performance/__main__.py
@@ -34,3 +34,67 @@ if __name__ == '__main__':
                     )
             except Exception as e:
                 print(f'Error running {test_module.name} with {bin_path.name}: {e}')
+
+
+
+
+
+
+# linux performance
+# test_np01.py
+#     python3.14
+#          proc_single              1.311957
+#          proc_thread_pool         3.796515
+#          proc_cond_thread_pool    1.306277
+#     python3.14t
+#          proc_single              1.331948
+#          proc_thread_pool         0.948886
+#          proc_cond_thread_pool    0.95085
+# test_np02.py
+#     python3.14
+#          proc_single              1.406858
+#          proc_thread_pool         4.444127
+#          proc_cond_thread_pool    1.479738
+#     python3.14t
+#          proc_single              1.435999
+#          proc_thread_pool         1.044917
+#          proc_cond_thread_pool    1.042418
+# test_np03.py
+#     python3.14
+#          proc_single              34.665409
+#          proc_thread_pool         37.356373
+#          proc_cond_thread_pool    29.089636
+#     python3.14t
+#          proc_single              29.086205
+#          proc_thread_pool         4.141262
+#          proc_cond_thread_pool    4.153254
+
+
+# macos
+# test_np01.py
+#     python3.14
+#          proc_single              0.519131
+#          proc_thread_pool         1.84927
+#          proc_cond_thread_pool    0.511398
+#     python3.14t
+#          proc_single              0.56307
+#          proc_thread_pool         0.411119
+#          proc_cond_thread_pool    0.40859
+# test_np02.py
+#     python3.14
+#          proc_single              0.615515
+#          proc_thread_pool         2.544554
+#          proc_cond_thread_pool    0.611674
+#     python3.14t
+#          proc_single              0.654962
+#          proc_thread_pool         0.50488
+#          proc_cond_thread_pool    0.496363
+# test_np03.py
+#     python3.14
+#          proc_single              17.16677
+#          proc_thread_pool         17.614034
+#          proc_cond_thread_pool    16.627494
+#     python3.14t
+#          proc_single              16.776953
+#          proc_thread_pool         2.596718
+#          proc_cond_thread_pool    2.611898
